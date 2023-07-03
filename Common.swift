bfb0bc75390c9741a25bb567d826a7cc0afebe90
ZE-R0-1 <jijiij77@naver.com>
2023-07-03 15:20:13 +0900

    Step5

---
+++ Common.swift
@@ -0,0 +1,10 @@
+//
+//  Common.swift
+//  WeatherPro
+//
+//  Created by 윤준영 on 2023/07/03.
+//
+
+import Foundation
+
+let key = "7vw1h1hOdvcvuBwkT5P9b%2FH8YMTlrBzxA9ICYl%2Br3k3ArTelwIHsD4FQ%2FGrYXriiDxff3A07xI9RNio%2BYoqV%2FQ%3D%3D"
